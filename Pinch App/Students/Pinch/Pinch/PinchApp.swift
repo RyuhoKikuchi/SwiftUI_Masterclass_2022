f9d01378a5ac0d5cb49142f344b57c5576789a93
RyuhoKikuchi <60661447+RyuhoKikuchi@users.noreply.github.com>
2022-08-17 14:14:16 +0900

    Section4 - 29 done

---
+++ Pinch App/Students/Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by 菊地竜帆 on 2022-08-17.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pinch App/Students/Pinch/Pinch/Screen/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by 菊地竜帆 on 2022-08-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    
+    // MARK: - FUNCTION
+    // MARK: - CONTENT
+    var body: some View {
+        NavigationView {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("thumb-magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 9, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(imageScale)
+                // MARK: - 1. TAP
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+            } //: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            })
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 13")
+    }
+}
